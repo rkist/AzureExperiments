46b94e3b24cddf6e6ff9feb5e976b1172d050215
Raul Kist <raulkist@gmail.com>
2017-06-18 23:38:12 +0200

    cleaning

--- DjangoWebProject1/DjangoWebProject1/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!e:\kist\documents\visual studio 2017\Projects\DjangoWebProject1\DjangoWebProject1\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
---
+++ Prototype01/Prototype01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Prototype01.MvcApplication" Language="C#" %>
